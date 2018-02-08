bea72193b49f39019226190f3ab9c6c248f73d6a
eleyne <eleynekrd@gmail.com>
2018-02-08 01:32:51 -0600

    Estructuración de carpetas y funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 let begin = () => {
-  let arrFriend = [
-  {id:'0', name: 'Aracely Gutarra', uri: 'AracelyGutarra.png'},
-  {id:'1', name: 'Brenda Mesias', url: 'BrendaMesias.png'},
-  {id:'2', name: 'daniela Pariona', uri: 'danielaPariona.png'},
-  {id:'3', name: 'Elizabeth Alcala', uri: 'ElizabethAlcala.png'},
-  {id:'4', name: 'jennifer Carmen', uri: 'jenniferCarmen.png'},
-  {id:'5', name: 'karina Ramirez', uri: 'karinaRamirez.png'},
-  {id:'6', name: 'Lesly Nomberto', uri: 'LeslyNomberto.png'},
-  {id:'7', name: 'Ornella Ysabel Campos', uri: 'OrnellaYsabelCampos.png'},
-  {id:'8', name: 'Pamela de la Cruz', uri: 'PameladelaCruz.png'},
-  {id:'9', name: 'yanira Arenazas', uri: 'yaniraArenazas.png'}];
-
+ 
   function imgRandom() {
     return arrFriend[parseInt(Math.random() * 10)];
   }
